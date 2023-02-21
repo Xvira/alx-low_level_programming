28cdf3b2eb92f32833fe14b0151bdf77547b4453
Xvira <kevinxvira@gmail.com>
2023-02-21 22:50:18 +0300

    from one point to

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*print_to_98 - prints from the integer to 98.
+*
+*@n: from this to 98.
+*
+*Return: null.
+*/
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
