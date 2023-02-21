86087556ef37446ec1537305abc40b3a7ecf3ef0
Xvira <kevinxvira@gmail.com>
2023-02-21 18:14:08 +0300

    Alphabet lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+void print_alphabet(void);
+
+/**
+ *
+ *main - entry point to the code
+ *
+ *Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 97; i != 123; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
