83c47a858e30f56f0cea43f2f5005f4d396cc2a9
Xvira <kevinxvira@gmail.com>
2023-02-23 10:37:58 +0300

    largest no

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
-#ifdef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
-#endif /* MAIN_H */
+#endif
