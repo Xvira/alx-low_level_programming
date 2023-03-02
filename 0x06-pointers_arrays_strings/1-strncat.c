67b031c81eb5d8d958934c957b4266676bd48625
Xvira <kevinxvira@gmail.com>
2023-03-02 23:40:36 +0300

    Concatenates

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *_strncat - concaterates two string
+ *
+ *@dest: string
+ *@src: sring 2
+ *@n: integer
+ *
+ *Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, k;
+
+	i = 0;
+	k = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[k] != '\0' && k < n)
+	{
+		dest[i] = src[k]
+			i++;
+		k++;
+
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+
+}
