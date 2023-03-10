32ba5f97856dbcc863cc80e1c7000f056d9ee067
Xvira <kevinxvira@gmail.com>
2023-03-10 22:05:33 +0300

    prints name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count of arguments.
+ * @argv: array of arguments.
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
