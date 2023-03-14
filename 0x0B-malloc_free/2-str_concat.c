9faa7eace3b04b7732a54cf2ba8dd4deada1e75f
Xvira <kevinxvira@gmail.com>
2023-03-15 00:55:14 +0300

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,27 +12,37 @@ char *str_concat(char *s1, char *s2)
 	char *con;
 	unsigned int len1 = 0, len2 = 0, size = 0, i = 0, a = 0;
 
-	while (s1[len1])
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s1 && s1[len1])
 		len1++;
-	while (s2[len2])
+
+	while (s2 && s2[len2])
 		len2++;
+
 	size = len1 + len2;
-	con = malloc(sizeof(char) * (size + 2));
-	while (i < len1)
+	con = malloc(sizeof(char) * (size + 1));
+	if (con == NULL)
+		return (NULL);
+	if (s1)
 	{
-		con[i] = s1[i];
-		i++;
+		while (i < len1)
+		{
+			con[i] = s1[i];
+			i++;
+		}
 	}
-	if (s2 == NULL)
+	if (s2)
 	{
-		con[i] = '\0';
-		return (con);
-	}
-	while (s2[a] != '\0')
-	{
-		con[i] = s2[a];
-		i++;
-		a++;
+		while (s2[a] != '\0')
+		{
+			con[i] = s2[a];
+			i++;
+			a++;
+		}
 	}
 	con[i] = '\0';
 	return (con);
